8ab489b5338e297bb6deac2bfe048f6599bd14bf
Tiago Santana <159819303+S4NTTANA@users.noreply.github.com>
2025-09-15 21:50:05 -0300

    Delete projetinho directory

--- projetinho/src/main/java/com/senai/projetinho/exception/EmailDuplicadoException.java
+++
@@ -1,7 +0,0 @@
-package com.senai.projetinho.exception;
-
-public class EmailDuplicadoException extends RuntimeException {
-    public EmailDuplicadoException(String message) {
-        super(message);
-    }
-}
--- projetinho/src/main/java/com/senai/projetinho/model/Sexo.java
+++
@@ -1,7 +0,0 @@
-package com.senai.projetinho.model;
-
-public enum Sexo {
-    MASCULINO,
-    FEMININO,
-    NAO_INFORMADO
-}
